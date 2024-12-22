0b292c34de1f2b0578d06cb8c5251f50a3d31d31
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2024-12-22 15:54:05 +0600

    Friends responsive done

--- src/assets/pages/Components/FriendRequest.jsx
+++ src/assets/pages/Components/FriendRequest.jsx
@@ -6,13 +6,13 @@ const FriendRequest = ({profile , name , time , mutual}) => {
   return (
     <div className='flex gap-x-4 items-center relative bg-clrthird/10 hover:bg-clrthird/20 p-2 rounded-md'>
         <p className="absolute top-1 right-1 text-clrthird">{time} ago</p>
-        <img src={profile} alt="profile" className='w-[80px] h-[80px] rounded-full object-cover  '/>
+        <img src={profile} alt="profile" className='lg:w-[80px] lg:h-[80px] w-[60px] h-[60px] rounded-full object-cover  '/>
         <div className="">
           <h5 className='font-aldrich  '>{name}</h5>
           <p className="font-ubuntu text-clrthird ">{mutual} <span>mutual</span></p>
           <div className="flex gap-x-3">
-            <button type='button' className='font-ubuntu duration-[.4s] text-clrthird py-1 px-5 bg-clrthird/10 rounded-md hover:bg-brand hover:text-primarytxt capitalize font-medium   '>confirm</button>
-            <button type='button' className='font-ubuntu duration-[.4s] text-clrthird py-1 px-5 bg-clrthird/10 rounded-md hover:bg-brand hover:text-primarytxt capitalize font-medium   '>delete</button>
+            <button type='button' className='font-ubuntu duration-[.4s] text-clrthird py-1 lg:px-5 px-2 lg:text-lg text-[15px] bg-clrthird/10 rounded-md hover:bg-brand hover:text-primarytxt capitalize font-medium   '>confirm</button>
+            <button type='button' className='font-ubuntu duration-[.4s] text-clrthird py-1 lg:px-5 px-2 lg:text-lg text-[15px] bg-clrthird/10 rounded-md hover:bg-brand hover:text-primarytxt capitalize font-medium   '>delete</button>
           </div>
         </div>
     </div>
--- src/assets/pages/Friends.jsx
+++ src/assets/pages/Friends.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router";
 import Nav from "./Components/Nav";
 import Menu from "./Components/Menu";
 import FriendRequest from "./Components/FriendRequest";
+import AddFriend from "./Components/AddFriend";
+import ViewSent from "./Components/ViewSent";
 
 // Icons
 // import { BsSearch } from "react-icons/bs";
@@ -31,6 +33,7 @@ const Friends = () => {
     let requestFn = ()=>{
       setRequest(true);
       setAdd(false);
+      setViewsent(false);
     }
 
     // add function
@@ -38,30 +41,38 @@ const Friends = () => {
     let addFn = ()=>{
       setRequest(false);
       setAdd(true);
+      setViewsent(false);
     }
 
 
+    // view sent function
+    let [viewsent , setViewsent] = useState(false);
+    let viwsentFn = ()=>{
+      setRequest(false);
+      setAdd(false);
+      setViewsent(true);
+    }
 
 
   return (
     <>
     <Nav menu={MenuVisiblity} home={()=> navigate('/home')} />
-    <Menu memu={memu}/>
+    <Menu memu={memu} menuClose={() => setMenu(false)}/>
     {/* Friends Start */}
     <section>
       <div className="container">
-        <div className="mx-[20%] p-2">
-          <h2 className="font-aldrich font-semibold text-2xl   ">Friends</h2>
+        <div className="lg:mx-[20%] mx-0 p-2">
+          <h2 className="font-aldrich font-semibold lg:text-2xl text-lg   ">Friends</h2>
           <div className="flex justify-between my-2">
             <button type="button" onClick={requestFn} className={`frndBtn ${request ? 'bg-brand text-primarytxt ' : 'bg-clrthird/10'} `}><RiUserReceivedLine/><span>Requests</span></button>
             <button type="button" onClick={addFn} className={`frndBtn ${add ? 'bg-brand text-primarytxt ' : 'bg-clrthird/10'} `}><RiUserAddLine/><span>Add</span></button>
-            <button type="button" className="frndBtn"><RiUserShared2Line/><span>View sent</span></button>
+            <button type="button" onClick={viwsentFn} className={`frndBtn ${viewsent ? 'bg-brand text-primarytxt ' : 'bg-clrthird/10'} `}><RiUserShared2Line/><span>View sent</span></button>
           </div>
           <div className="py-2">
             <h4 className="font-aldrich font-semibold  ">Import contacts</h4>
             <div className="flex justify-between items-center">
-              <p className="font-ubuntu text-clrthird max-w-[70%] ">Let Ochigram automatically uploads new and updated contacts as you add them to your phone</p>
-              <button type="button" className="py-2 px-5 bg-clrthird/10 hover:text-primarytxt font-medium hover:bg-brand duration-[.4s] rounded-md  ">Get started</button>
+              <p className="font-ubuntu text-clrthird max-w-[65%] ">Let Ochigram automatically uploads new and updated contacts as you add them to your phone</p>
+              <button type="button" className="lg:py-2 py-1 lg:px-5 px-3 bg-clrthird/10 hover:text-primarytxt font-medium hover:bg-brand duration-[.4s] rounded-md  ">Get started</button>
             </div>
           </div>
           <h3 className="font-aldrich text-lg  ">People you may know</h3>
@@ -73,6 +84,22 @@ const Friends = () => {
               <FriendRequest profile={profile} name='Md. Shakhawat' mutual='12' time='2d'   />
               <FriendRequest profile={profile} name='Md. Shakhawat' mutual='12' time='2d'   />
             </div>
+            }
+            {add &&
+              <div className="flex flex-col gap-y-2">
+                <AddFriend profile={profile} name='Md. Shakhawat' mutual='12' time='2d'/>
+                <AddFriend profile={profile} name='Md. Shakhawat' mutual='12' time='2d'/>
+                <AddFriend profile={profile} name='Md. Shakhawat' mutual='12' time='2d'/>
+                <AddFriend profile={profile} name='Md. Shakhawat' mutual='12' time='2d'/>
+              </div> 
+            }
+            {viewsent && 
+              <div className="flex flex-col gap-y-2">
+                <ViewSent profile={profile} name='Md. Shakhawat' mutual='12' time='2d'/>
+                 <ViewSent profile={profile} name='Md. Shakhawat' mutual='12' time='2d'/>
+                <ViewSent profile={profile} name='Md. Shakhawat' mutual='12' time='2d'/>
+                <ViewSent profile={profile} name='Md. Shakhawat' mutual='12' time='2d'/>
+              </div> 
             }
           </div>
         </div>
