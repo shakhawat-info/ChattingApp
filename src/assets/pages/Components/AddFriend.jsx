15242510e2c7170ba044ee33740621f40712a10d
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2025-01-11 13:36:48 +0600

    UserList & Sent updated

--- src/assets/pages/Components/AddFriend.jsx
+++ src/assets/pages/Components/AddFriend.jsx
@@ -1,99 +1,89 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { getDatabase, ref , set , onValue, push } from "firebase/database";
+import { getDatabase, ref, set, onValue, push } from "firebase/database";
 
 const AddFriend = () => {
-  // Database reference
   const db = getDatabase();
-
-  // Current user from Redux store
   const currentUser = useSelector((state) => state.userInfo.value);
-  // console.log(currentUser);
-  
-
-  // State for storing user list
-  let [userList, setUserList] = useState([]);
-  let [updatedlist , setUpdatedlist] = useState([]);
-  let [requestlist , setRequestlist] = useState([]);
-
-  // added item
+  const [userList, setUserList] = useState([]);
 
   useEffect(() => {
     const userListRef = ref(db, "users/");
+    const FriendRef = ref(db, "FriendRequests/");
 
-    // Fetch users from database
+    // Fetch users
     onValue(userListRef, (snapshot) => {
       const listedUsers = [];
       snapshot.forEach((item) => {
-        // console.log(item);
-        
-        if (currentUser.user.uid !== item.key ) {
-          listedUsers.push({user:item.val() , uid: item.key , status: false});
+        if (currentUser.user.uid !== item.key) {
+          listedUsers.push({ user: item.val(), uid: item.key });
         }
       });
-      setUserList(listedUsers);
+
+      // Fetch friend requests
+      onValue(FriendRef, (snapshot) => {
+        const requestArr = [];
+        snapshot.forEach((item) => {
+          requestArr.push(item.val());
+        });
+
+        // Filter users who haven't been sent friend requests
+        const filteredUsers = listedUsers.filter(
+          (userItem) =>
+            !requestArr.some(
+              (requestItem) => requestItem.receiver.uid === userItem.uid
+            )
+        );
+        // Update the user list
+        setUserList(filteredUsers); 
+      });
     });
-  }, []);
+  }, [currentUser]);
 
-
-
-  // Add friends
-  let add = (item)=>{
-    
-    set(push(ref(db, 'FriendRequests/')), {
+  const add = (item) => {
+    set(push(ref(db, "FriendRequests/")), {
       sender: currentUser.user,
-      receiver: item
-    })
-
-
-    const FriendRef = ref(db, 'FriendRequests/');
-    onValue(FriendRef, (snapshot) => {
-      let requestArr = [];
-
-      snapshot.forEach((item)=>{
-        requestArr.push(item.val());
-      })
-      
-      // requestArr.map((item)=>{
-      //   console.log(item.receiver);
-        
-      // })
-      userList.map((item)=>{
-        
-      })
-      
-      
+      receiver: item,
     });
-    
-  }
 
+    // Filter the user list to remove the added user
+    setUserList((prevList) =>
+      prevList.filter((userItem) => userItem.uid !== item.uid)
+    );
+  };
 
-  
   return (
     <div className="flex flex-col gap-y-2">
-      {
-      userList.map((item) => (
-        <div key={item.uid} className="flex gap-x-4 items-center relative bg-clrthird/10 hover:bg-clrthird/20 p-2 rounded-md">
+      {userList.map((item) => (
+        <div
+          key={item.uid}
+          className="flex gap-x-4 items-center relative bg-clrthird/10 hover:bg-clrthird/20 p-2 rounded-md"
+        >
           <p className="absolute top-1 right-1 text-clrthird"> ago</p>
-          <img src={item.user.photoURL} alt="profile" className="lg:w-[80px] w-[60px] lg:h-[80px] h-[60px] rounded-full object-cover" />
+          <img
+            src={item.user.photoURL}
+            alt="profile"
+            className="lg:w-[80px] w-[60px] lg:h-[80px] h-[60px] rounded-full object-cover"
+          />
           <div>
             <h5 className="font-aldrich">{item.user.displayName}</h5>
-            <p className="font-ubuntu text-clrthird"> <span>mutual</span> </p>
+            <p className="font-ubuntu text-clrthird">
+              <span>mutual</span>
+            </p>
             <div className="flex gap-x-3">
-              {!item.status ?
-              <>
-              <button type="button" onClick={()=>add(item)}   className="font-ubuntu duration-[.4s] py-1 lg:px-5 px-2 text-[15px] lg:text-lg rounded-md bg-brand text-primarytxt capitalize font-medium">
+              <button
+                type="button"
+                onClick={() => add(item)}
+                className="font-ubuntu duration-[.4s] py-1 lg:px-5 px-2 text-[15px] lg:text-lg rounded-md bg-brand text-primarytxt capitalize font-medium"
+              >
                 Add
               </button>
-              <button type="button" className="font-ubuntu duration-[.4s] text-clrthird py-1 lg:px-5 px-2 text-[15px] lg:text-lg bg-clrthird/10 rounded-md capitalize font-medium">
+              <button
+                type="button"
+                className="font-ubuntu duration-[.4s] text-clrthird py-1 lg:px-5 px-2 text-[15px] lg:text-lg bg-clrthird/10 rounded-md capitalize font-medium"
+              >
                 Remove
               </button>
-              </>
-              :
-              <button type="button" className="font-ubuntu duration-[.4s] text-clrthird py-1 lg:px-5 px-2 text-[15px] lg:text-lg bg-clrthird/10 rounded-md capitalize font-medium">
-                Cancel
-              </button>
-              }
             </div>
           </div>
         </div>
--- src/assets/pages/Components/ViewSent.jsx
+++ src/assets/pages/Components/ViewSent.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, remove } from "firebase/database";
 
 const ViewSent = () => {
   const db = getDatabase();
@@ -15,24 +15,30 @@ const ViewSent = () => {
       let requestArr = [];
       snapshot.forEach((item)=>{
         if(item.val().sender.uid == currentUser.user.uid){
-          requestArr.push(item.val().receiver);
+          requestArr.push(item);
         }
         setRequests(requestArr);
       })
     });
   } , [])
 
+  let Cancel = (id)=>{
+    // console.log(requests);
+    remove(ref(db, 'FriendRequests/' + id.key))
+    
+    
+  }
   return (
     <div className="flex flex-col-reverse gap-y-2">
       {requests.map((item , index)=>{
         return (<div key={index} className='flex gap-x-4 items-center relative bg-clrthird/10 hover:bg-clrthird/20 p-2 rounded-md'>
         <p className="absolute top-1 right-1 text-clrthird"> ago</p>
-        <img src={item.user.photoURL} alt="profile" className='lg:w-[80px] w-[60px] lg:h-[80px] h-[60px] rounded-full object-cover  '/>
+        <img src={item.val().receiver.user.photoURL} alt="profile" className='lg:w-[80px] w-[60px] lg:h-[80px] h-[60px] rounded-full object-cover  '/>
         <div className="">
-          <h5 className='font-aldrich  '>{item.user.displayName}</h5>
+          <h5 className='font-aldrich  '>{item.val().receiver.user.displayName}</h5>
           <p className="font-ubuntu text-clrthird "> <span>mutual</span></p>
           <div className="flex gap-x-3">
-            <button type='button' className='font-ubuntu duration-[.4s] lg:text-lg text-[15px]  py-1 px-2 lg:px-5  rounded-md bg-brand text-primarytxt capitalize font-medium   '>cancel</button>
+            <button type='button' onClick={()=>Cancel(item)} className='font-ubuntu duration-[.4s] lg:text-lg text-[15px]  py-1 px-2 lg:px-5  rounded-md bg-brand text-primarytxt capitalize font-medium   '>cancel</button>
           </div>
         </div>
         </div>)
